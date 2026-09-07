d739ff3260b6af1c39fe279a74882e4858b62dc8
benjbread <benjbreadon@gmail.com>
2026-09-07 03:24:23 -0700

    test

---
+++ src/sysinv/services.py
@@ -0,0 +1,23 @@
+from .helpers import run_command
+
+
+def detect_software(OS: str):
+    # services_list: list[dict] = []
+
+    # Create empty result dict that will hold wether it was a success (bool) and the data_or_reason and be returned
+    result_dict: dict = {}
+    if OS == "Windows":
+        import psutil
+
+        print(psutil.win_service_iter())
+
+    elif OS == "Linux":
+        pass
+
+    elif OS == "Darwin":
+        pass
+
+    else:
+        result_dict["success"] = False
+        result_dict["data_or_reason"] = "OS not yet implemented"
+        return result_dict
